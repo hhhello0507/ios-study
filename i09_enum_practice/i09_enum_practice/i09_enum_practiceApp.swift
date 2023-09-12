fae578bae32a54d69ba833e2da60d9a8a945ddf0
bswlkh0310 <hhhello0507@gmail.com>
2023-09-12 21:28:53 +0900

    ADD: enum practice

---
+++ i09_enum_practice/i09_enum_practice/i09_enum_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  i09_enum_practiceApp.swift
+//  i09_enum_practice
+//
+//  Created by dgsw8th71 on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct i09_enum_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
