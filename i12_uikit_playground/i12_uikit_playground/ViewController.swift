345dfe8c687282c3ef6a338465fa58bef7525003
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-12 19:18:29 +0900

    Init :: Playground project setting

---
+++ i12_uikit_playground/i12_uikit_playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i12_uikit_playground
+//
+//  Created by dgsw8th71 on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
