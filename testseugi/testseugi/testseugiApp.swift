47ad2219a680bd06bc78172cd4a7be53f8dcf705
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-29 01:02:31 +0900

    testseugi

---
+++ testseugi/testseugi/testseugiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testseugiApp.swift
+//  testseugi
+//
+//  Created by hhhello0507 on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct testseugiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
